553c439669f8808626d58484e6eb6868380504d7
Sandor Czettner <sandor@trellis.co>
2020-03-12 19:37:33 +0000

    Add flexshopper id

---
+++ Setup/UpgradeSchema.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace FlexShopper\Payments\Setup;
+
+class UpgradeSchema implements \Magento\Framework\Setup\UpgradeSchemaInterface
+{
+    const COLUMN_FLEXSHOPPER_ID = 'flexshopper_id';
+    /**
+     *
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function upgrade(
+        \Magento\Framework\Setup\SchemaSetupInterface $setup,
+        \Magento\Framework\Setup\ModuleContextInterface $context
+    ) {
+        $setup->startSetup();
+
+
+        if (version_compare($context->getVersion(), '1.1.0', '<')) {
+            $setup->getConnection()->addColumn(
+                $setup->getTable('quote'),
+                self::COLUMN_FLEXSHOPPER_ID,
+                [
+                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    'length' => 255,
+                    'nullable' => true,
+                    'comment' => 'Flexshopper ID'
+                ]
+            );
+
+            $setup->getConnection()->addColumn(
+                $setup->getTable('sales_order'),
+                self::COLUMN_FLEXSHOPPER_ID,
+                [
+                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    'length' => 255,
+                    'nullable' => true,
+                    'comment' => 'Flexshopper ID'
+                ]
+            );
+        }
+
+        $setup->endSetup();
+    }
+}
